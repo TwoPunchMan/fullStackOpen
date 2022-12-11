c8522522dc285e798bfac96ffd8490e73b2f4c7f
twopunchman <ray.wang.pro@gmail.com>
2022-12-11 01:54:32 -0500

    Exercises 3.1-3.8 phonebook-backend

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const morgan = require('morgan');
 const app = express();
 
-
 let phonebook = [
 	{
 		"id": 1,
@@ -27,6 +26,16 @@ let phonebook = [
 ]
 
 app.use(express.json());
+app.use(morgan((tokens, req, res) => {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+		JSON.stringify(req.body)
+  ].join(' ')
+}));
 
 app.get('/api/persons', (request, response) => {
 	response.json(phonebook);
