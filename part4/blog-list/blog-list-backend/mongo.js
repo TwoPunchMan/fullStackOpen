def1a8dee48d817bce79ab649432125d4229ed23
twopunchman <ray.wang.pro@gmail.com>
2022-12-18 20:52:20 -0500

    Deleted mongo.js/ not needed

--- part4/blog-list/blog-list-backend/mongo.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-const mongoose = require('mongoose');
-
-const Blog = require('./models/blog');
-
-if (process.argv.length < 3) {
-  console.log('Need a password as argument');
-	process.exit(1);
-}
